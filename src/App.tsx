38ed8c85e66477f915256db46fc19b3427cfc31a
Damaris Göbel <damaris.goebel@gmx.de>
2021-08-12 09:26:45 +0200

    deleted response component, used message component for own message sending

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,27 @@ import React from 'react'
 import Footer from './components/Footer/Footer'
 import Container from './components/Container/Container'
 import Message from './components/Message/Message'
-import Response from './components/Response/Response'
 
 import { getAllMessages } from './api/api'
 
 function App() {
   const [messages, setMessages] = React.useState<any>([])
-  const [response, setResponse] = React.useState<any>({})
 
   React.useEffect(() => {
     getAllMessages().then(function (result) {
-      let messageArrayWithoutLastOne = result.slice(0,-1);;
-      setMessages(messageArrayWithoutLastOne)
-      let lastOne = result[result.length - 1]
-      setResponse(lastOne)
+      setMessages(result)
     })
-
   }, [])
 
   return (
     <div>
       <Container>
+        <div>
         {messages.length > 0 && messages.map((message: any, index: number) => {
           return <Message key={index} author={message.author} message={message.message} timestamp={message.timestamp} />
         })
         }
-        {response !== {} &&  <Response message={response.message} timestamp={response.timestamp} />
-        }
-        
+        </div>
       </Container>
       <Footer />
     </div>
--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,34 +1,54 @@
 import React from 'react'
+import clsx from 'clsx'
 import styles from './Message.module.css'
 
 type Props = {
-    author: string;
-    message: string;
-    timestamp: number;
+  author: string
+  message: string
+  timestamp: number
 }
 
-
-export function timeConverter(timestamp: number){
-    let a = new Date(timestamp);
-    let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    let year = a.getFullYear();
-    let month = months[a.getMonth()];
-    let date = a.getDate();
-    let hour = a.getHours();
-    let min = a.getMinutes();
-    let time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min;
-    return time;
+export function timeConverter(timestamp: number) {
+  let a = new Date(timestamp)
+  let months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ]
+  let year = a.getFullYear()
+  let month = months[a.getMonth()]
+  let date = a.getDate()
+  let hour = a.getHours()
+  let min = a.getMinutes()
+  let time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min
+  return time
 }
 
-const Message: React.FC<Props> = ({author, message, timestamp}) => {
-    let convertedTimestamp = timeConverter(timestamp)
-    return (
-        <div className={styles.messageBubble}>
-            <p className={styles.smallText}>{author}</p>
-            <p className={styles.text}>{message}</p>
-            <p className={styles.smallText}>{convertedTimestamp}</p>
-        </div>
-    )
+const Message: React.FC<Props> = ({ author, message, timestamp }) => {
+  let convertedTimestamp = timeConverter(timestamp)
+  return (
+    <div className={styles.messageItem}>
+      <div
+        className={clsx(
+          styles.message,
+          author === 'Damaris' ? styles.messageOut : styles.messageIn,
+        )}
+      >
+        <p className={styles.smallText}>{author}</p>
+        <p className={styles.text}>{message}</p>
+        <p className={styles.smallText}>{convertedTimestamp}</p>
+      </div>
+    </div>
+  )
 }
 
-export default Message;
+export default Message
