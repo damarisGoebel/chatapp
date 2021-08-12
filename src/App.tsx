ef410106e9fc6ab0607297938ba20cff15cacc1d
Damaris Göbel <damaris.goebel@gmx.de>
2021-08-12 09:35:14 +0200

    auto scroll to the bottom with ref

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,18 @@ import Message from './components/Message/Message'
 import { getAllMessages } from './api/api'
 
 function App() {
+  const messagesEndRef = React.useRef(null)
+
+  const scrollToBottom = () => {
+    // @ts-ignore
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }
+
   const [messages, setMessages] = React.useState<any>([])
+
+  React.useEffect(() => {
+    scrollToBottom()
+  }, [messages])
 
   React.useEffect(() => {
     getAllMessages().then(function (result) {
@@ -18,11 +29,19 @@ function App() {
     <div>
       <Container>
         <div>
-        {messages.length > 0 && messages.map((message: any, index: number) => {
-          return <Message key={index} author={message.author} message={message.message} timestamp={message.timestamp} />
-        })
-        }
+          {messages.length > 0 &&
+            messages.map((message: any, index: number) => {
+              return (
+                <Message
+                  key={index}
+                  author={message.author}
+                  message={message.message}
+                  timestamp={message.timestamp}
+                />
+              )
+            })}
         </div>
+        <div ref={messagesEndRef} />
       </Container>
       <Footer />
     </div>
